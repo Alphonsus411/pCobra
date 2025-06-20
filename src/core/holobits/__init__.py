78170107bba0d99bd9dce11c27c6509d011ed83d
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-06-20 11:51:24 +0200

    Add Holobit class and placeholder functions

---
+++ src/core/holobits/__init__.py
@@ -0,0 +1,6 @@
+from .holobit import Holobit
+from .graficar import graficar
+from .proyection import proyectar
+from .transformacion import transformar
+
+__all__ = ["Holobit", "graficar", "proyectar", "transformar"]
---
+++ src/core/holobits/holobit.py
@@ -0,0 +1,16 @@
+class Holobit:
+    """Representa un conjunto de valores numericos para operaciones 3D/2D."""
+
+    def __init__(self, valores):
+        if not hasattr(valores, "__iter__"):
+            raise TypeError("'valores' debe ser iterable")
+        self.valores = [float(v) for v in valores]
+
+    def __repr__(self):
+        return f"Holobit({self.valores})"
+
+    def __len__(self):
+        return len(self.valores)
+
+    def __getitem__(self, index):
+        return self.valores[index]
