2464af8b359ac098fb7b8164f9dff26428b1cd62
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-06-26 11:03:37 +0200

    Bump version to 1.3

--- backend/src/jupyter_kernel/__init__.py
+++ backend/src/jupyter_kernel/__init__.py
@@ -16,9 +16,9 @@ def install(user=True):
 
 class CobraKernel(Kernel):
     implementation = "Cobra"
-    implementation_version = "1.2"
+    implementation_version = "1.3"
     language = "cobra"
-    language_version = "1.2"
+    language_version = "1.3"
     language_info = {
         "name": "cobra",
         "mimetype": "text/plain",
