9a2a7149d72d5d0fa3e7864b2a7f5809b3fa20cc
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-06-28 12:16:16 +0200

    Add parser conditional and macro error tests

--- backend/src/tests/test_parser_errors_extra.py
+++ backend/src/tests/test_parser_errors_extra.py
@@ -1,6 +1,6 @@
 import pytest
-from src.cobra.lexico.lexer import Lexer
-from src.cobra.parser.parser import Parser
+from backend.src.cobra.lexico.lexer import Lexer
+from backend.src.cobra.parser.parser import Parser
 
 
 def parse(code: str):
@@ -22,5 +22,21 @@ def test_desde_without_import():
 
 def test_unclosed_macro():
     codigo = "macro m { var x = 1 "
+    with pytest.raises(SyntaxError):
+        parse(codigo).parsear()
+
+def test_condicional_sino_sin_fin():
+    codigo = """
+    si x > 0:
+        imprimir(x)
+    sino:
+        imprimir(x)
+    """
+    with pytest.raises(SyntaxError):
+        parse(codigo).parsear()
+
+
+def test_macro_llaves_desbalanceadas():
+    codigo = "macro m { var x = 1 }}"
     with pytest.raises(SyntaxError):
         parse(codigo).parsear()
