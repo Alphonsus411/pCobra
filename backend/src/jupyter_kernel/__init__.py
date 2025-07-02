c5f682c4ffb13f803fba088e7b94be72c7378d35
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-07-02 11:54:00 +0200

    Update bump_version script and files

--- backend/src/jupyter_kernel/__init__.py
+++ backend/src/jupyter_kernel/__init__.py
@@ -16,9 +16,9 @@ def install(user=True):
 
 class CobraKernel(Kernel):
     implementation = "Cobra"
-    implementation_version = "5.6"
+    implementation_version = "5.6.1"
     language = "cobra"
-    language_version = "5.6"
+    language_version = "5.6.1"
     language_info = {
         "name": "cobra",
         "mimetype": "text/plain",
