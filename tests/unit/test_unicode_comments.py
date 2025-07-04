31c41dbc39149c8b752c2716b8805c886b3695c3
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-07-04 08:02:50 +0200

    Add test for unicode comments removal

---
+++ tests/unit/test_unicode_comments.py
@@ -0,0 +1,24 @@
+from src.cobra.lexico.lexer import Lexer, TipoToken
+
+
+def test_unicode_comments_removed():
+    codigo = """
+    # пример
+    var x = 1
+    // مثال
+    imprimir(x)
+    /* مثال */
+    """
+    tokens = Lexer(codigo).analizar_token()
+
+    assert [(t.tipo, t.valor) for t in tokens] == [
+        (TipoToken.VAR, "var"),
+        (TipoToken.IDENTIFICADOR, "x"),
+        (TipoToken.ASIGNAR, "="),
+        (TipoToken.ENTERO, 1),
+        (TipoToken.IMPRIMIR, "imprimir"),
+        (TipoToken.LPAREN, "("),
+        (TipoToken.IDENTIFICADOR, "x"),
+        (TipoToken.RPAREN, ")"),
+        (TipoToken.EOF, None),
+    ]
