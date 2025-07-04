cedfbdab572f959db341aa6662ac55c324a458c6
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-07-04 14:38:51 +0200

    feat: soporte para transpiladores externos

--- examples/plugins/setup.py
+++ examples/plugins/setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 setup(
     name='ejemplo-plugin-cobra',
     version='1.0',
-    py_modules=['saludo_plugin', 'md2cobra_plugin'],
+    py_modules=['saludo_plugin', 'md2cobra_plugin', 'transpiler_demo'],
     entry_points={
         'cobra.plugins': [
             'saludo = saludo_plugin:SaludoCommand',
             'md2cobra = md2cobra_plugin:MarkdownToCobraCommand',
+        ],
+        'cobra.transpilers': [
+            'demo = transpiler_demo:TranspiladorDemo',
         ],
     },
 )
