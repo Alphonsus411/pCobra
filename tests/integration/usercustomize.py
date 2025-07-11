4a99a70842b16b17d371f09c390d91dff06a233e
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-07-11 15:36:30 +0200

    Add CLI integration tests with pexpect

---
+++ tests/integration/usercustomize.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+from subprocess import CompletedProcess
+
+if os.environ.get("PEXPECT_TESTING"):
+    def fake_run(cmd, *args, **kwargs):
+        print(f"FAKE_RUN {cmd[0]}")
+        return CompletedProcess(cmd, 0)
+    subprocess.run = fake_run
+    try:
+        import backend.src.core.sandbox as sandbox
+
+        def fake_sandbox(code: str) -> None:
+            print("hola")
+
+        sandbox.ejecutar_en_sandbox = fake_sandbox
+    except Exception:
+        pass
