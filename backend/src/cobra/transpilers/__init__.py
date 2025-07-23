0f68140fe9f10fb50090ad2d4a8799de11b6ffe5
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-07-23 19:00:33 +0200

    feat: add reverse transpiler base

--- backend/src/cobra/transpilers/__init__.py
+++ backend/src/cobra/transpilers/__init__.py
@@ -1,5 +1,6 @@
 """Facilita el acceso a los distintos transpiladores de Cobra."""
 
 from src.cobra.transpilers.base import BaseTranspiler
+from src.cobra.transpilers.reverse.base import BaseReverseTranspiler
 
-__all__ = ["BaseTranspiler"]
+__all__ = ["BaseTranspiler", "BaseReverseTranspiler"]
---
+++ backend/src/cobra/transpilers/reverse/__init__.py
@@ -0,0 +1,5 @@
+"""Componentes para convertir código de otros lenguajes a Cobra."""
+
+from src.cobra.transpilers.reverse.base import BaseReverseTranspiler
+
+__all__ = ["BaseReverseTranspiler"]
