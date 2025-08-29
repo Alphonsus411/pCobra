a5abb888e0c6c7c4ebec46523ea3bd07ed5c15b0
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-08-29 14:39:13 +0200

    Agregar pruebas unitarias para el lexer

---
+++ tests/test_lexer.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+import pytest
+
+sys.path.append(str(Path(__file__).resolve().parent.parent / "pCobra"))
+from cobra.core.lexer import InvalidTokenError, Lexer, TipoToken
+
+
+
+def test_lexer_aritmetica_simple() -> None:
+    codigo = "1 + 2"
+    lexer = Lexer(codigo)
+    tokens = lexer.tokenizar()
+    tipos = [token.tipo for token in tokens]
+    assert tipos == [
+        TipoToken.ENTERO,
+        TipoToken.SUMA,
+        TipoToken.ENTERO,
+        TipoToken.EOF,
+    ]
+
+
+def test_lexer_token_invalido() -> None:
+    with pytest.raises(InvalidTokenError):
+        lexer = Lexer("$")
+        lexer.tokenizar()
