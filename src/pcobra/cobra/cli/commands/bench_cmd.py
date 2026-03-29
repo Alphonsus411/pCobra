8453dce97ebe0fbf429934ce6edd38e6cd632c26
Adolfo González Hernández <adolfogonzal@gmail.com>
2026-03-29 18:55:18 +0200

    fix(bench): normaliza import de targets_policy en bench_cmd

--- src/pcobra/cobra/cli/commands/bench_cmd.py
+++ src/pcobra/cobra/cli/commands/bench_cmd.py
@@ -30,7 +30,10 @@ from pcobra.cobra.cli.utils.messages import mostrar_error, mostrar_info
 from pcobra.core.cobra_config import tiempo_max_transpilacion
 from pcobra.cobra.transpilers.target_utils import target_label
 from pcobra.cobra.transpilers.targets import OFFICIAL_TARGETS
-from pcobra.cobra.benchmarks.targets_policy import BENCHMARK_BACKEND_METADATA, validate_backend_metadata
+from pcobra.cobra.benchmarks.targets_policy import (
+    BENCHMARK_BACKEND_METADATA,
+    validate_backend_metadata,
+)
 
 # Constantes
 ANSI_ESCAPE = re.compile(r"\x1b\[[0-9;]*m")
