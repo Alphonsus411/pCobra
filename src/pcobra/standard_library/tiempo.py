fff3a61150dffc3f934e662c1a5aae655cee8fd3
Adolfo González Hernández <adolfogonzal@gmail.com>
2026-05-12 11:04:48 +0200

    Fix missing corelibs module aliases in stdlib wrappers

--- src/pcobra/standard_library/tiempo.py
+++ src/pcobra/standard_library/tiempo.py
@@ -1,5 +1,6 @@
 """Fachada temporal en español para `usar \"tiempo\"`."""
 
+import pcobra.corelibs.tiempo as _tiempo
 from pcobra.corelibs.tiempo import ahora, formatear, dormir, epoch, desde_epoch
 
 __all__ = ["ahora", "formatear", "dormir", "epoch", "desde_epoch"]
